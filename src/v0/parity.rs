78320991122bbc425f745dac509cbf2357d94327
Hoani Bryson <hoani.bryson@gmail.com>
2020-04-22 16:07:54 +1200

    V0 Encode/Decode Bug Fixes and Refactors (#44)
    * Variable renames and remove mask magic numbers
    * payload_version and signature_version allocations are strictly enforced
    * payload_version decode mask bug fixed
    * signature_version decode mask bug fixed
    * domain_key overflow bug fixed

--- src/v0/parity.rs
+++ src/v0/parity.rs
@@ -20,6 +20,8 @@ use crate::traits::DoughnutApi;
 
 const NOT_BEFORE_MASK: u8 = 0b1000_0000;
 const SIGNATURE_MASK: u8 = 0b0001_1111;
+const VERSION_UPPER_MASK: u8 = 0b0000_0111;
+const VERSION_11BIT_MASK: u16 = 0b0000_0111_1111_1111;
 
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub struct DoughnutV0 {
@@ -37,10 +39,11 @@ impl DoughnutV0 {
     /// Encodes the doughnut into an byte array and writes the result into a given memory
     /// if `encode_signature` is false, the final signature bytes are not included in the result
     fn encode_to_with_signature_optional<T: Output>(&self, dest: &mut T, encode_signature: bool) {
-        let mut payload_version_and_signature_version = self.payload_version.swap_bits();
+        let mut payload_version_and_signature_version =
+            (self.payload_version & VERSION_11BIT_MASK).swap_bits();
 
         payload_version_and_signature_version |=
-            u16::from(self.signature_version).swap_bits() >> 11;
+            u16::from(self.signature_version & SIGNATURE_MASK).swap_bits() >> 11;
         dest.write(&payload_version_and_signature_version.to_be_bytes());
 
         let mut domain_count_and_not_before_byte =
@@ -65,7 +68,8 @@ impl DoughnutV0 {
         // Write permission domain headers
         for (key, payload) in &self.domains {
             let mut key_buf = [0_u8; 16];
-            key_buf[..key.len()].clone_from_slice(&key.as_bytes());
+            let length = key_buf.len().min(key.len());
+            key_buf[..length].clone_from_slice(&key.as_bytes()[..length]);
             dest.write(&key_buf);
             for b in &(payload.len() as u16).to_le_bytes() {
                 dest.push_byte(b.swap_bits());
@@ -137,12 +141,13 @@ impl DoughnutApi for DoughnutV0 {
 
 impl Decode for DoughnutV0 {
     fn decode<I: Input>(input: &mut I) -> Result<Self, codec::Error> {
-        let payload_byte_0 = input.read_byte()?.swap_bits();
-        let payload_byte_1 = input.read_byte()?.swap_bits();
+        let version_byte_0 = input.read_byte()?.swap_bits();
+        let version_byte_1 = input.read_byte()?.swap_bits();
 
-        let payload_version = u16::from_le_bytes([payload_byte_0, payload_byte_1 & 0b1110_0000]);
+        let payload_version =
+            u16::from_le_bytes([version_byte_0, version_byte_1 & VERSION_UPPER_MASK]);
 
-        let signature_version = (payload_byte_1 & SIGNATURE_MASK) >> 3;
+        let signature_version = (version_byte_1 & SIGNATURE_MASK.swap_bits()) >> 3;
 
         let domain_count_and_not_before_byte = input.read_byte()?;
         let permission_domain_count = (domain_count_and_not_before_byte.swap_bits() >> 1) + 1;
@@ -329,5 +334,98 @@ mod test {
             doughnut.validate(holder, u64::max_value()),
             Err(ValidationError::Conversion)
         )
+    }
+
+    #[test]
+    fn versions_encode_and_decode() {
+        let holder = [1_u8; 32];
+        let doughnut = DoughnutV0 {
+            issuer: [0_u8; 32],
+            holder,
+            domains: vec![("TestDomain".to_string(), vec![])],
+            expiry: 0,
+            not_before: 0,
+            payload_version: 0x0515,
+            signature_version: 0x1a,
+            signature: H512::default(),
+        };
+
+        let parsed_doughnut = DoughnutV0::decode(&mut &doughnut.encode()[..]).unwrap();
+        assert_eq!(parsed_doughnut.signature_version, 0x1a);
+        assert_eq!(parsed_doughnut.payload_version, 0x0515);
+    }
+
+    #[test]
+    fn payload_version_does_not_cross_contaminate() {
+        let holder = [1_u8; 32];
+        let doughnut = DoughnutV0 {
+            issuer: [0_u8; 32],
+            holder,
+            domains: vec![("TestDomain".to_string(), vec![])],
+            expiry: 0,
+            not_before: 0,
+            payload_version: 0xffff,
+            signature_version: 0x00,
+            signature: H512::default(),
+        };
+
+        let parsed_doughnut = DoughnutV0::decode(&mut &doughnut.encode()[..]).unwrap();
+        assert_eq!(parsed_doughnut.signature_version, 0x00);
+        assert_eq!(parsed_doughnut.payload_version, VERSION_11BIT_MASK);
+    }
+
+    #[test]
+    fn signature_version_does_not_cross_contaminate() {
+        let holder = [1_u8; 32];
+        let doughnut = DoughnutV0 {
+            issuer: [0_u8; 32],
+            holder,
+            domains: vec![("TestDomain".to_string(), vec![])],
+            expiry: 0,
+            not_before: 0,
+            payload_version: 0x0000,
+            signature_version: 0xff,
+            signature: H512::default(),
+        };
+
+        let parsed_doughnut = DoughnutV0::decode(&mut &doughnut.encode()[..]).unwrap();
+        assert_eq!(parsed_doughnut.payload_version, 0x0000);
+        assert_eq!(parsed_doughnut.signature_version, 0x1f);
+    }
+
+    #[test]
+    fn short_domain_name_is_parsed() {
+        let holder = [1_u8; 32];
+        let doughnut = DoughnutV0 {
+            issuer: [0_u8; 32],
+            holder,
+            domains: vec![("Smol".to_string(), vec![])],
+            expiry: 0,
+            not_before: 0,
+            payload_version: 0x0000,
+            signature_version: 0xff,
+            signature: H512::default(),
+        };
+
+        let parsed_doughnut = DoughnutV0::decode(&mut &doughnut.encode()[..]).unwrap();
+        assert_eq!(parsed_doughnut.domains[0].0, "Smol");
+    }
+
+    #[test]
+    fn long_domain_name_is_truncated() {
+        let holder = [1_u8; 32];
+        let doughnut = DoughnutV0 {
+            issuer: [0_u8; 32],
+            holder,
+            domains: vec![("SweetLikeAChic-a-CherryCola".to_string(), vec![])],
+            expiry: 0,
+            not_before: 0,
+            payload_version: 0x0000,
+            signature_version: 0xff,
+            signature: H512::default(),
+        };
+
+        let parsed_doughnut = DoughnutV0::decode(&mut &doughnut.encode()[..]).unwrap();
+        assert_eq!(parsed_doughnut.domains[0].0, "SweetLikeAChic-a");
     }
 }
