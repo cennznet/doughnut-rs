c12ed4425a1974849cf60ed65704821e6a74e4c6
Ben Gao <xbengao@gmail.com>
2020-07-21 16:52:55 +1200

    Introduce webpack

---
+++ js/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: '#cheap-module-eval-source-map',
+  entry: './libWeb/doughnut.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.html/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.wasm$/,
+        use: ['wasm-loader']
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.template.html'
+    }),
+    new WasmPackPlugin({
+      crateDirectory: path.resolve(__dirname, ".")
+    }),
+    new webpack.ProvidePlugin({
+      TextDecoder: ['text-encoding', 'TextDecoder'],
+      TextEncoder: ['text-encoding', 'TextEncoder']
+    })
+  ],
+};
