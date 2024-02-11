09283d99495a6d83acbdbfd4dc74417ddfa12b6c
zees-dev <dev.786zshan@gmail.com>
2024-02-12 10:50:04 +1300

    added back no_std support

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 
 #![warn(clippy::pedantic)]
 #![allow(clippy::must_use_candidate, clippy::missing_errors_doc)]
-
+#![cfg_attr(not(feature = "std"), no_std)]
 extern crate alloc;
 
 pub mod doughnut;
